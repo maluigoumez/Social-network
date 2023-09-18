ecfbb27ea81c1522e1837a54eac3eacd233c7f4e
Ariadna Rodriguez <ariadna.rodriguez30@hotmail.com>
2023-09-18 17:06:29 -0600

    Agregando home.js y login.js

--- src/home.js
+++ src/home.js
@@ -1,13 +1,16 @@
-
-export default home;
-
-function home(){
+function home(navigateTo){
     const section = document.createElement('section');
     const title = document.createElement('h2');
+    const buttonLogin = document.createElement('button');
+    buttonLogin.innerHTML = 'ir a Login';
+    buttonLogin.addEventListener('click', () => {
+        navigateTo('/login')
+    })
 
     title.textContent = "Welcome"
-    section.append(title)
+    section.append(title, buttonLogin);
     return section;
 
 
 }
+export { home };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { start } from './srtar.js';
-import { home } from './home.js'
+import {login} from './login.js';
+import  {home}  from './home.js'
 
 const root = document.getElementById('root');
-root.append(home());
+const routes = [
+    { path: '/', component: home},
+    { path: '/login', component: login},
+];
+
+const defaultRoute = '/';
+
+function navigateTo(hash){
+
+    console.log({ hash })
+    const route = routes.find((routeFind)=>{
+        return routeFind.path === hash;
+    })
+
+    if(route && route.component){
+        window.history.pushState(
+            {},
+            route.path,
+            window.location.origin + route.path,
+        );
+        console.log("CHECK ROUTE ", route);
+        root.innerHTML = '';
+        root.appendChild(route.component(navigateTo));
+    }
+
+}
+
+navigateTo(window.location.pathname);
 
