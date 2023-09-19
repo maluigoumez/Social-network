16b4187cbf68d445cf8db37039dd2bbd4422f326
Ariadna Rodriguez <ariadna.rodriguez30@hotmail.com>
2023-09-19 13:46:33 -0600

    nueva implementacion de spa sin error

--- src/main.js
+++ src/main.js
@@ -1,39 +1,19 @@
-// file main.js finished
-import home from './home.js';
-import login from './login.js';
-import error from './error.js';
+import { addRoutes, onNavigate } from './router/index.js';
+import { home } from './pages/home.js';
+import { register } from './pages/register.js';
+import { wall } from './pages/wall.js';
 
-const routes = [
-  { path: '/', component: home },
-  { path: '/login', component: login },
-  { path: '/error', component: error },
-];
+addRoutes({
+  '/': home,
+  '/register': register,
+  '/wall': wall,
+});
 
-const defaultRoute = '/';
-const root = document.getElementById('root');
-// console.log('ok');
-function navigateTo(hash) {
-  console.log(hash);
-  const route = routes.find((routeFound) => routeFound.path === hash);
-
-  if (route && route.component) {
-    window.history.pushState(
-      {},
-      route.path,
-      window.location.origin + route.path,
-    );
-
-    if (root.firstChild) {
-      root.removeChild(root.firstChild);
-    }
-    root.appendChild(route.component(navigateTo));
-  } else {
-    navigateTo('/error');
-  }
-}
+// Lógica de la aplicacion
+window.onload = () => {
+  onNavigate(window.location.pathname);
+};
 
 window.onpopstate = () => {
-  navigateTo(window.location.pathname);
+  onNavigate(window.location.pathname);
 };
-console.log(window.location);
-navigateTo(window.location.pathname || defaultRoute);
