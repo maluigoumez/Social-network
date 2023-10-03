fe14745a9b60c922934c7e09d6159af510ae3823
Malui Gomez <luisagomezrodela@gmail.com>
2023-10-03 14:44:37 -0600

    successful google login takes you to feed.js

--- src/lib/googleLogin.js
+++ src/lib/googleLogin.js
@@ -6,13 +6,28 @@ export function callGoogle() {
   signInWithPopup(auth, provider)
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+      // // The signed-in user info.
+      // const user = result.user;
+      // // IdP data available using getAdditionalUserInfo(result)
+      // // ...
+      // })
+
+      // Handle successful login here
+      // Access user information from the result object
       const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
+      const displayName = user.displayName;
+      const email = user.email;
+      const uid = user.uid;
+
+      // You can now perform actions based on the user's information
+      console.log(`Successful login for user: ${displayName} (${email})`);
+
+      // Redirect to the feed.js file upon successful login
+      window.location.href = "./pages/feed.js"; // You may need to adjust the path
     })
+
     .catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
@@ -22,6 +37,7 @@ export function callGoogle() {
       // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
+      console.error("Google login error:", error);
     });
   return {};
 }
